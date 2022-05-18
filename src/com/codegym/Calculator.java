f584fb43a3a94eb5869ced3461ebe61e976f501d
anhtay <tayphongtran@gmail.com>
2022-05-18 16:30:39 +0700

    up

---
+++ src/com/codegym/Calculator.java
@@ -0,0 +1,28 @@
+package com.codegym;
+
+public class Calculator {
+
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
+
+
+    public static int calculate(int  firstOperand, int  secondOperand, char operator) {
+        switch (o) {
+            case ADDITION:
+                return  firstOperand +  secondOperand;
+            case SUBTRACTION:
+                return  firstOperand -  secondOperand;
+            case MULTIPLICATION:
+                return  firstOperand *  secondOperand;
+            case DIVISION:
+                if ( secondOperand != 0)
+                    return  firstOperand /  secondOperand;
+                else
+                    throw new RuntimeException("Can not divide by 0");
+            default:
+                throw new RuntimeException("Unsupported operation");
+        }
+    }
+}
